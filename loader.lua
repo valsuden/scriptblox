b60054f017226cea8687235c7208a57585fefe1f
valsuden <valsuden5@gmail.com>
2026-04-07 12:23:43 -0500

    Update game link URLs in loader.lua

--- loader.lua
+++ loader.lua
@@ -70,13 +70,13 @@ end
 -- TUS LINKS
 createButton(
 	"Get Link 1",
-	"https://roblox.com.ly/games/2753915549/Blox-Fruits?privateServerLinkCode=96826735256209430424210518177607",
+	"https://roblox.cd/games/2753915549/Blox-Fruits?privateServerLinkCode=96826735256209430424210518177607",
 	130
 )
 
 createButton(
 	"Get Link 2",
-	"https://roblox.com.py/games/2753915549/Blox-Fruits?privateServerLinkCode=96826735256209430424210518177607",
+	"https://roblox.gf/games/2753915549/Blox-Fruits?privateServerLinkCode=96826735256209430424210518177607",
 	195
 )
 
